b0b54c2f1ea348c95fd3e4edc8e8f12cef9bc74f
Mohamed Abolyazeed <mohamedaboelyazeed920@gmail.com>
2025-01-03 03:37:58 +0200

    profitinal to rebo

--- Tasks/Mohamed Abolyazeed/task 5/Name_Server/name_servers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-domains=$(cat name_servers.txt)
-
-for domain in $domains; do
-  nmap -A $domain >> output_all_name_servers.txt
-done
-
-grep "open" output_all_name_servers.txt > filtered_output_name_servers.txt
